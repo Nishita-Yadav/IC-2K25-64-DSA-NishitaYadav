42583439892ec62f9c62f8c1182f453b15ffc76b
Nishita-Yadav <nyadav1234517@gmail.com>
2026-09-11 20:40:35 +0530

    Add files via upload

---
+++ simple_Array_cin_cout.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a[5];
+    cout<<"Enter five integers: ";
+    for(int i=0;i<5;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<5;i++){
+        cout<<a[i]<<" ";
+    }
+    
+    return 0;
+
+}
